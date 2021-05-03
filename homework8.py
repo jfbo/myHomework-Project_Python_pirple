c518ef05c529a2385d74952223e3d3082cd43732
jfbo <74642174+jfbo@users.noreply.github.com>
2021-05-03 14:56:02 +0800

    Update homework8.py
    Added homework instructions

--- homework8.py
+++ homework8.py
@@ -8,6 +8,14 @@
 Homework # 8 : I/O
 Creating a Note-taking Program
 '''
+#Create a note-taking program. When a user starts it up, it should prompt them for a filename.
+#If they enter a file name that doesn't exist, it should prompt them to enter the text they want to write to the file. 
+#After they enter the text, it should save the file and exit.
+#If they enter a file name that already exists, it should ask the user if they want:
+#A) Read the file | B) Delete the file and start over | C) Append the file
+#If the user wants to read the file it should simply show the contents of the file on the screen. 
+#If the user wants to start over then the file should be deleted and another empty one made in its place. 
+#If a user elects to append the file, then they should be able to enter more text, and that text should be added to the existing text in the file. 
 
 
 '''
