58ba9e993d7d5f36951e77af94653d9df0afa8ba
jfbo <74642174+jfbo@users.noreply.github.com>
2021-05-03 14:38:20 +0800

    Update homework1.py
    Added homework instructions as comments

--- homework1.py
+++ homework1.py
@@ -8,6 +8,15 @@
 Homework # 1 : Variables
 What's your Favorite Song
 """
+#Details:
+#What's your favorite song?
+#Think of all the attributes that you could use to describe that song. That is: all of it's details or "meta-data". 
+#These are attributes like "Artist", "Year Released", "Genre", "Duration", etc. Think of as many different characteristics as you can.
+#List all of the attributes of the song, one after another, by creating variables for each attribute, and giving each variable a value.
+#For extra credit, add comments to your code, that explain the different attributes.
+#Also add a title to the top of your file explaining what the file is, and what it's for.
+
+
 
 #Data with String Values
 Artist="Ed Sheeran"
