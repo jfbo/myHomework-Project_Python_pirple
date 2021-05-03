bab553cf4f083152f57d40e29c938287d6eab5eb
jfbo <74642174+jfbo@users.noreply.github.com>
2021-05-03 14:54:09 +0800

    Update homework7.py
    Added homework instructions

--- homework7.py
+++ homework7.py
@@ -7,6 +7,12 @@
 '''
 Homework # 7 : Dictionaries and Sets
 '''
+#Return to your first homework assignments, when you described your favorite song. Refactor that code so all the variables are held as dictionary keys and value. 
+#Then refactor your print statements so that it's a single loop that passes through each item in the dictionary and prints out it's key and then it's value.
+#Create a function that allows someone to guess the value of any key in the dictionary, and find out if they were right or wrong. Parameters: Key and Value
+#If the key exists in the dictionary and that value is the correct value, then the function should return true. In all other cases, it should return false.
+
+
 
 song = {
     "artist": "Ed Sheeran", 
