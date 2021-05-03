e886e0f8af8fcff10c2ce948609b3b9bdcd3509c
jfbo <74642174+jfbo@users.noreply.github.com>
2021-05-03 14:58:40 +0800

    Update project1.py
    Added project instructions

--- project1.py
+++ project1.py
@@ -2,6 +2,14 @@
 Project # 1 : A Simple Game | CONNECT 4
 Connect 4 Game by Hasbro Company (using python)
 '''
+#https://youtu.be/utXzIFEVPjA 
+#Draw the board, and allow two players to take turns placing their pieces on the board (but as you learned above, they can only do so by choosing a column, not a row). 
+#The first player to get 4 across or diagonal should win! (Normally the pieces would be red and black, but you can use X and O instead)
+#Want to try colorful pieces instead of X and O? First you'll need to figure out how to import a package like termcolor into your project. 
+#We're going to cover importing later in the course, but try and see if you can figure it out on your own. Or you might be able to find unicode characters to use instead, 
+#depending on what your system supports. Here's a hint: print(u'\u2B24')
+
+
 
 import sys
 from termcolor import colored, cprint
