de806b92d7699f26d998579dadd9b55c7a2c9da0
jfbo <74642174+jfbo@users.noreply.github.com>
2021-05-03 15:08:29 +0800

    Update project3.py
    Added project instructions

--- project3.py
+++ project3.py
@@ -1,6 +1,10 @@
 """
 Project # 3 : Pick A Card Game!
 """
+#Everyone has their favorite card game. What's yours? For this assignment, choose a card game (other than Blackjack), and turn it into a Python program. 
+#Game should ask for the players' names. The game should refer to them by name only. ("It's John's turn" instead of "It's player 1's turn). 
+#At any point during the game, someone should be able to type "--help" to be taken to a screen where they can read the rules of the game and instructions for how to play. 
+#After they're done reading, they should be able to type "--resume" to go back to the game and pick up where they left off.
 
 '''
 "War" Card Game
